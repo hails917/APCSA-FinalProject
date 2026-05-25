0faaad62ee02a444514bf7d0820b4e36e7cb8e6b
hails917 <hcabral621@gmail.com>
2026-05-25 19:36:59 -0400

    Update Location.java

--- Location.java
+++ Location.java
@@ -1,4 +1,67 @@
 public class Location
 {
-    // Copy over your Location class here
+    //Implement the Location class here
+    private boolean isShipAtLoc;
+    private int status;
+    
+    public static final int UNGUESSED = 0;
+    public static final int HIT = 1;
+    public static final int MISSED = 2;
+    
+    public Location(){
+        status = UNGUESSED;
+        isShipAtLoc = false;
+    }
+    
+    public boolean checkHit(){
+        if (status == HIT){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public boolean checkMiss(){
+        if (status == MISSED){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public boolean isUnguessed(){
+        if (status == UNGUESSED){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public void markHit(){
+        status = HIT;
+    }
+    
+    public void markMiss(){
+        status = MISSED;
+    }
+    
+    public boolean hasShip(){
+        if (isShipAtLoc == true){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public void setShip(boolean val){
+        isShipAtLoc = val;
+    }
+    
+    public void setStatus(int status){
+        this.status = status;
+    }
+    
+    public int getStatus(){
+        return status;
+    }
 }
